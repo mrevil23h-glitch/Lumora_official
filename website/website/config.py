b49fa9868a5f24193f3c0519d89a1ba9e2e43d68
mrevil23h-glitch <mr.evil23h@gmail.com>
2026-08-03 19:29:07 +0000

    Add configuration settings for Lumora website

---
+++ website/website/config.py
@@ -0,0 +1,13 @@
+APP_NAME = "Lumora"
+
+
+# Support du site
+SUPPORT_ADDRESS = "https://t.me/VOTRE_SUPPORT"
+
+
+# Paiement crypto
+CRYPTO_ADDRESS = "VOTRE_ADRESSE_CRYPTO"
+
+
+# Prix Premium
+PREMIUM_PRICE = 10000
