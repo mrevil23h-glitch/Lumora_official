61e69f6fc60b2c28dba01c7fcf5ca56d1d0bc957
mrevil23h-glitch <mr.evil23h@gmail.com>
2026-08-03 19:08:12 +0000

    Create messages.py

---
+++ messages.py
@@ -0,0 +1,16 @@
+WELCOME = """
+🌙 Bienvenue sur Lumora
+
+Une plateforme avec des services gratuits et Premium.
+
+Choisis une option.
+"""
+
+
+PREMIUM = """
+⭐ Lumora Premium
+
+Prix : 10 000
+
+Paiement crypto disponible.
+"""
